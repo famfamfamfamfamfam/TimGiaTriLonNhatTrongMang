935870ab6582026d7e400180e8df206968dd1ded
BinhTran <fam.out16@gmail.com>
2024-08-13 06:49:40 +0700

    Add cac files cua bai thuc hanh tim gia tri lon nhat cua mang

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MANGMOTCHIEU
+{
+    class TimPhanTuLonNhatTrongMang
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so ty phu co khoi tai san can so sanh:");
+            byte n = byte.Parse(Console.ReadLine());
+            if (n>20)
+            {
+                Console.WriteLine("Vuot qua so luong co the xet");
+                return;
+            }
+            long[] danhsachtaisan = new long[n];
+            long max = danhsachtaisan[0];
+            int vitri = 1;
+            for(int i = 0; i<n; i++)
+            {
+                Console.WriteLine("Nhap tai san cua nguoi thu {0}: ", i+1);
+                danhsachtaisan[i] = int.Parse(Console.ReadLine());
+                if(danhsachtaisan[i]>max)
+                {
+                    max = danhsachtaisan[i];
+                    vitri = i + 1;
+                }
+            }
+            Console.WriteLine("Tai san lon nhat co gia tri {0} thuoc ve vi thu {1}", max, vitri);
+        }
+    }
+}
